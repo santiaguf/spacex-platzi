5119e229e2496eda149782b8fc48729a56b8ab2e
Santiago Bernal <exactlimon@gmail.com>
2021-03-31 17:01:21 -0500

    using eslint

---
+++ js/launches.js
@@ -0,0 +1,121 @@
+function getApiData(api,selector) {
+    var requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+    };
+
+    fetch(api, requestOptions)
+    .then(response => response.json())
+    .then(result => printLaunch(result, selector))
+    .catch(error => console.log('error', error));
+}
+
+function printLaunch(result, selector) {
+    const title = document.querySelector('#title'+selector);
+    title.textContent = `${result.name}`;
+
+    const img = document.querySelector('#img'+selector);
+    let imgUrl = `${result.links.patch.small}`;
+
+    if(imgUrl == 'null' || imgUrl == null){
+        imgUrl = 'img/astronauta.png'
+        img.setAttribute('width','60%');
+    }
+
+    img.setAttribute('src', imgUrl);
+
+    const date = document.querySelector('#date'+selector);
+    date.textContent = `${result.date_local}`;
+
+    const moreInfo = document.querySelector('#more'+selector);
+    moreInfo.setAttribute('href','launch.html?id='+`${result.id}`)
+
+    if(selector == '-upcoming'){
+        getCountDownTimer(result.date_local);
+    }
+}
+
+function getCountDownTimer(launchDate){
+    // Set the date we're counting down to
+    var countDownDate = new Date(launchDate).getTime();
+    // Update the count down every 1 second
+    var x = setInterval(function() {
+
+    // Get today's date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Output the result in an element with id="countdown-upcoming"
+    document.getElementById('countdown-upcoming').innerHTML = days + 'days ' + hours + 'hours ' + minutes + 'minutes ' + seconds + 'seconds ';
+
+    // If the count down is over, write some text
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById('countdown-upcoming').innerHTML = 'EXPIRED';
+    }
+    }, 1000);
+}
+
+function getPastLaunches(api){
+    var requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+    };
+
+    fetch(api, requestOptions)
+    .then(response => response.json())
+    .then(result => printPastLaunchesList(result))
+    .catch(error => console.log('error', error));
+}
+
+function printPastLaunchesList(result){
+    const launchesDiv = document.querySelector('#past-launches');
+    for(let count in result){
+        launchesDiv.appendChild(createElement(result[count], count));
+    }
+}
+
+function createElement(launch, count){
+    const div = document.createElement('div');
+    div.setAttribute('id', 'card-'+count);
+    div.setAttribute('class','col-sm-6');
+    div.style.display = 'inline-block';
+
+    const link = document.createElement('a');
+    link.setAttribute('id', 'link-'+count);
+    link.setAttribute('class', 'badge badge-secondary');
+    link.setAttribute('href', 'launch.html?id='+`${launch.id}`);
+    div.appendChild(link);
+
+    let launchNumber =  parseInt(count)+1;
+
+    const paragraph = document.createElement('p');
+    paragraph.setAttribute('id', 'item-'+count);
+    paragraph.textContent = launchNumber+". "+`${launch.name}`;
+    link.appendChild(paragraph);
+
+    return div;
+}
+
+const apiBaseUrl = 'https://api.spacexdata.com/v4/';
+
+const upcomingApi = apiBaseUrl+'launches/next';
+const upcomingSelector = '-upcoming';
+
+const latestApi = apiBaseUrl+'launches/latest';
+const latestSelector = '-latest';
+
+const pastLaunchesApi = apiBaseUrl + 'launches/past';
+
+getApiData(upcomingApi,upcomingSelector);
+getApiData(latestApi,latestSelector);
+
+getPastLaunches(pastLaunchesApi);
